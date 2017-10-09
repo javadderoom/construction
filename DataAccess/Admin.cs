29f4c2ce7cbfc2eee468695c130f0cb99ef06da6
javadderoom <deroom24@gmail.com>
2017-10-09 21:00:14 +0330

    heh

--- DataAccess/Admin.cs
+++ DataAccess/Admin.cs
@@ -12,12 +12,10 @@ namespace DataAccess
     using System;
     using System.Collections.Generic;
     
-    public partial class sysdiagram
+    public partial class Admin
     {
-        public string name { get; set; }
-        public int principal_id { get; set; }
-        public int diagram_id { get; set; }
-        public Nullable<int> version { get; set; }
-        public byte[] definition { get; set; }
+        public int AdminID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }

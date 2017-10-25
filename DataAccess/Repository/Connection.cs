c94f9f86c9bb8b2bce245c00699c450531dbf490
javadderoom <deroom24@gmail.com>
2017-10-26 00:35:03 +0330

    awd

---
+++ DataAccess/Repository/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    internal class Connection
+    {
+        public ConstructionCompanyEntities GetContext()
+        {
+            ConstructionCompanyEntities wt = new ConstructionCompanyEntities();
+            return wt;
+        }
+    }
+}
--- WebPages/_construction/Index.aspx.designer.cs
+++ WebPages/_construction/Index.aspx.designer.cs
@@ -13,6 +13,60 @@ namespace WebPages._construction {
     public partial class Index1 {
         
         /// <summary>
+        /// servisDetails control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl servisDetails;
+        
+        /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
+        /// updatepanel2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel updatepanel2;
+        
+        /// <summary>
+        /// servisContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl servisContent;
+        
+        /// <summary>
+        /// UpdatePanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// ourServises control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl ourServises;
+        
+        /// <summary>
         /// blogsContainer control.
         /// </summary>
         /// <remarks>

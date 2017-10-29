86704f590b4bcb338a6ec536bddcacdeadf6b532
HAMOON <hamoonshahsavar@gmail.com>
2017-10-29 23:38:16 +0330

    123

--- WebPages/_construction/Index.aspx.designer.cs
+++ WebPages/_construction/Index.aspx.designer.cs
@@ -184,6 +184,15 @@ namespace WebPages._construction {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl ourServises;
         
         /// <summary>
+        /// projects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl projects;
+        
+        /// <summary>
         /// blogsContainer control.
         /// </summary>
         /// <remarks>

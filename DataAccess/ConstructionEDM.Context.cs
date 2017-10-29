7d95ac83d58097795c8d787895b60929739a5805
prgrmmr <sasan.parsafar@yahoo.com>
2017-10-29 20:22:14 +0330

    20:22

--- DataAccess/ConstructionEDM.Context.cs
+++ DataAccess/ConstructionEDM.Context.cs
@@ -25,6 +25,8 @@ namespace DataAccess
             throw new UnintentionalCodeFirstException();
         }
     
+        public virtual DbSet<ProjectConnection> ProjectConnections { get; set; }
+        public virtual DbSet<ProjectGroup> ProjectGroups { get; set; }
         public virtual DbSet<Admin> Admins { get; set; }
         public virtual DbSet<Article> Articles { get; set; }
         public virtual DbSet<Chat> Chats { get; set; }
@@ -40,8 +42,6 @@ namespace DataAccess
         public virtual DbSet<Link> Links { get; set; }
         public virtual DbSet<Message> Messages { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
-        public virtual DbSet<ProjectConnection> ProjectConnections { get; set; }
-        public virtual DbSet<ProjectGroup> ProjectGroups { get; set; }
         public virtual DbSet<Project> Projects { get; set; }
         public virtual DbSet<Satisfaction> Satisfactions { get; set; }
         public virtual DbSet<Slider> Sliders { get; set; }

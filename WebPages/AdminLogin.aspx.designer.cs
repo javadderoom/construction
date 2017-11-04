b838213e31f59ea72c574ca4ee22300ddb07756c
javadderoom <deroom24@gmail.com>
2017-11-04 20:52:05 +0330

    aaaaa

--- WebPages/AdminLogin.aspx.designer.cs
+++ WebPages/AdminLogin.aspx.designer.cs
@@ -101,23 +101,5 @@ namespace WebPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button BtnLogin;
-        
-        /// <summary>
-        /// lblWarning control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblWarning;
-        
-        /// <summary>
-        /// SpLabl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl SpLabl;
     }
 }

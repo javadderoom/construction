aa7d5a8cbb89512d48a03eacf9cb25fc4964c48d
javadderoom <deroom24@gmail.com>
2017-11-04 14:15:32 +0330

    یشی

--- WebPages/_construction/js/construction.js
+++ WebPages/_construction/js/construction.js
@@ -22,6 +22,8 @@ $(document).ready(function () {
             }
             $('#imageName').html(filename);
         });
+        //equal height
+
         $('#ContentPlaceHolder1_fileResume').change(function () {
             var filename = $(this).val();
             var lastIndex = filename.lastIndexOf("\\");
@@ -233,4 +235,40 @@ $(document).ready(function () {
             shadow: 0
         })
     })(jQuery)
+});
+equalheight = function (container) {
+    var currentTallest = 0,
+         currentRowStart = 0,
+         rowDivs = new Array(),
+         $el,
+         topPosition = 0;
+    $(container).each(function () {
+        $el = $(this);
+        $($el).height('auto')
+        topPostion = $el.position().top;
+
+        if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+    });
+}
+
+$(window).load(function () {
+    equalheight('.projects .project');
+});
+
+$(window).resize(function () {
+    equalheight('.projects .project');
 });
